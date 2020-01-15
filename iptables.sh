db1de6043b9272e18dbeb5d5c2235cfe14e0c846
Kosala Hemachandra <kvhnuke@aol.com>
2020-01-15 07:15:05 -0800

    test commit

--- iptables.sh
+++ iptables.sh
@@ -1,7 +1,6 @@
-sudo iptables -L
-# sudo iptables -I INPUT 1 -i lo -j ACCEPT
-# sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-# sudo iptables -A OUTPUT -p tcp -d registry.npmjs.org --dport 443 -j ACCEPT
-# sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-# sudo iptables -P INPUT DROP
-# sudo iptables -P OUTPUT DROP
+sudo iptables -I INPUT 1 -i lo -j ACCEPT
+sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -d registry.npmjs.org --dport 443 -j ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
